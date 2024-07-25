0fe40de34b5abd65ea7baa59c3c3244b4a61a5e7
Bhudati Vineela <Bhudati.Vineela@godigit.com>
2024-07-25 16:52:29 +0530

    Classes and Objects of Java

---
+++ classesandobjects/Rectangle.java
@@ -0,0 +1,32 @@
+package classesandobjects;
+
+public class Rectangle {
+    public int length;
+    public int width;
+    public Rectangle(int length,int width){
+        this.length=length;
+        this.width=width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getArea(){
+        return length*width;
+    }
+    public int getPerimeter(){
+        return 2*(length+width);
+    }
+}
---
+++ classesandobjects/Student.java
@@ -0,0 +1,39 @@
+package classesandobjects;
+
+public class Student {
+    public int StudentId;
+    public String name;
+    public int age;
+    public String grade;
+
+    public int getStudentId() {
+        return StudentId;
+    }
+
+    public void setStudentId(int studentId) {
+        StudentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
